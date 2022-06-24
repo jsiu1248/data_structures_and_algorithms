fa72a2bbd9349d6e7b824645b3d6d84a706960b6
Jonathan Siu <jsiu1248@gmail.com>
2022-06-23 22:02:29 -0700

    adding node and pros and cons

---
+++ linked_lists/linked_list_notes.py
@@ -0,0 +1,17 @@
+"""A linked list is a series of connected nodes.
+It has a multiple nodes, which each node has a value and pointer/next
+Pros:
+Ease of Insertion and Deletion
+Not set size
+
+Con:
+Have to start from the head node
+Extra memory is needed
+Not cache friendly
+
+"""
+
+class Node:
+    def __init__(self, value):
+        self.value = value # Assign data/value
+        self.next = None # next starts off as null
